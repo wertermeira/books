5e654ad6ceabce3c93453e6152017ecfba760654
Werter Meira <wertermeira@hotmail.com>
2017-09-17 17:12:17 -0300

    request ok

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe "Books", type: :request do
   describe "GET /books" do
     it "works! (now write some real specs)" do
+      set_password = Faker::Internet.password
+      user = User.create!(name: Faker::Name.name, email: Faker::Internet.email  ,password: set_password, password_confirmation: set_password, admin: true, status: true)
+      sign_in user
       get books_path
       expect(response).to have_http_status(200)
     end
